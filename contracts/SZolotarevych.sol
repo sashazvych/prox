e9ed692b575514c5168adefe14c0d23cb93e2ef1
sashazvych <94450178+sashazvych@users.noreply.github.com>
2023-03-25 01:00:55 -0400

    Add files via upload

---
+++ contracts/SZolotarevych.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract SZolotarevych is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+
+    uint256 public count;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC721_init("SZolotarevych", "SZLT");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = ++count;
+        _safeMint(to, tokenId);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+    internal
+    onlyOwner
+    override
+    {}
+}
